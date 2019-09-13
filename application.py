847cca25e9791e41f597c5918c3c9a38cd996ffe
Matteo Filannino <matteo.filannino@claremont.se>
2019-09-13 13:34:08 +0200

    commit error

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
-import logging
-import logging.handlers
-from wsgiref.simple_server import make_server
+##import logging
+##import logging.handlers
+##from wsgiref.simple_server import make_server
 
 
 # Create logger
@@ -156,5 +156,4 @@ if __name__ == '__main__':
     httpd = make_server('', 8000, application)
     print("Serving on port 8000...")
     httpd.serve_forever()
-    if sadajf
-        
+  
